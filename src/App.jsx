4e1d819887f7478bb93594acb5c0c1bd71eb0139
Donggeun <202310037@post.au.dk>
2025-10-13 12:47:45 +0200

    hallo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GamblingCrash from './components/GamblingCrash'
+import App from './App'
 
-export default function App() {
+export default function Main() {
   return (
-    <div>
-      <GamblingCrash />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
