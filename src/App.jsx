110e2676a706b4f782b18dc8c75f57d36ebb3c0d
Donggeun <202310037@post.au.dk>
2025-10-15 11:06:47 +0200

    tilbage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import GamblingCrash from './components/GamblingCrash'
 
 export default function App() {
-  return (
-    <div>
-    <GamblingCrash />
-    <GrowthComparison />
-    </div>
-  );
+  return <GamblingCrash />
 }
