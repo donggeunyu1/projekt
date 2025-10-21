50ddaa6df9a743268342d2fd89e5f823cbb13636
Donggeun <202310037@post.au.dk>
2025-10-21 09:37:31 +0200

    bias til 1

--- src/components/CrashGame/gameComponents/GamblingCrash.jsx
+++ src/components/CrashGame/gameComponents/GamblingCrash.jsx
@@ -30,7 +30,7 @@ function GamblingCrash() {
   }
 
   function generateCrashPoint() {
-  const bias = 13;
+  const bias = 1;
   const r = Math.random();
   const val = -Math.log(1 - r) / bias;
   return Math.max(1.03, Math.round(val * 10) / 10);
